76118fe1d7f23fb38d2083cf41aee7b944122c96
LeDaniii <danielmatzer@gmail.com>
2021-01-24 23:00:34 +0100

    vue-router start

---
+++ ninja-jobs/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, dolores eos sint corporis, rerum sunt facilis ea vel at ab odio quibusdam quo tempore sequi corrupti ipsa minus unde? Ipsum magnam cum voluptatum enim provident corrupti accusantium laboriosam aut nam. Possimus commodi, officia tempora laborum sit aliquam ad dolore et?</p>
+  </div>
+</template>
---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto quidem cum qui vitae natus nemo dolorem, veritatis rerum itaque corrupti cumque sapiente exercitationem. Iste voluptatem excepturi molestiae eveniet eaque ut. Autem alias dolorum quia facilis modi perspiciatis iste, necessitatibus quis quibusdam nihil accusantium blanditiis. Officiis tempora atque inventore, dolorum sequi cumque dignissimos illo expedita culpa perspiciatis harum cum voluptatibus alias illum, velit ut nihil natus reiciendis ex deserunt iste omnis. Corrupti delectus quod labore corporis? Exercitationem consequatur assumenda, corporis aliquam totam facere expedita eveniet illo sint mollitia magnam explicabo tempora quidem reiciendis nesciunt rem doloribus itaque id. Aut, sapiente voluptas?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto quidem cum qui vitae natus nemo dolorem, veritatis rerum itaque corrupti cumque sapiente exercitationem. Iste voluptatem excepturi molestiae eveniet eaque ut. Autem alias dolorum quia facilis modi perspiciatis iste, necessitatibus quis quibusdam nihil accusantium blanditiis. Officiis tempora atque inventore, dolorum sequi cumque dignissimos illo expedita culpa perspiciatis harum cum voluptatibus alias illum, velit ut nihil natus reiciendis ex deserunt iste omnis. Corrupti delectus quod labore corporis? Exercitationem consequatur assumenda, corporis aliquam totam facere expedita eveniet illo sint mollitia magnam explicabo tempora quidem reiciendis nesciunt rem doloribus itaque id. Aut, sapiente voluptas?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto quidem cum qui vitae natus nemo dolorem, veritatis rerum itaque corrupti cumque sapiente exercitationem. Iste voluptatem excepturi molestiae eveniet eaque ut. Autem alias dolorum quia facilis modi perspiciatis iste, necessitatibus quis quibusdam nihil accusantium blanditiis. Officiis tempora atque inventore, dolorum sequi cumque dignissimos illo expedita culpa perspiciatis harum cum voluptatibus alias illum, velit ut nihil natus reiciendis ex deserunt iste omnis. Corrupti delectus quod labore corporis? Exercitationem consequatur assumenda, corporis aliquam totam facere expedita eveniet illo sint mollitia magnam explicabo tempora quidem reiciendis nesciunt rem doloribus itaque id. Aut, sapiente voluptas?</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
