d5bcc8cd5e2ad4cc32e179621d021b8219a71085
LeDaniii <danielmatzer@gmail.com>
2021-01-24 21:48:39 +0100

    sign up form finish

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
       <label>Email:</label>
       <input type="email" required v-model="email">
 
       <label>Password:</label>
       <input type="password" required v-model="password">
+      <div class="error" v-if="passwordError">{{passwordError}}</div>
 
       <label>Role:</label>
       <br>
@@ -12,6 +13,13 @@
           <option value="developer">Web Developer</option>
           <option value="designer">Web Designer</option>
       </select>
+
+      <label>Skills:</label>
+      <input type="text" v-model="tempSkill" @keydown="addSkill">
+      <div class="pill" v-for="skill in skills" :key="skill">
+          <span @click="erase(skill)">{{skill}}</span>
+      </div>
+
 
       <div class="terms">
           <input type="checkbox" required v-model="terms">
@@ -33,11 +41,14 @@
           <input type="checkbox" value="Vicky" v-model="names">
           <label>Vicky</label>
       </div> -->
+      <div class="submit">
+          <button>Create an Account</button>
+      </div>
   </form>
-  <p>Your emial: {{email}}</p>
+  <!-- <p>Your emial: {{email}}</p>
   <p>Your password: {{password}}</p>
   <p>Your role: {{role}}</p>
-  <p>Terms accepted: {{terms}}</p>
+  <p>Terms accepted: {{terms}}</p> -->
   <!-- <p>Names: {{names}}</p> -->
 </template>
 
@@ -49,10 +60,39 @@ data() {
         password: '',
         role: 'developer',
         terms: false,
+        tempSkill: '',
+        skills:[],
+        passwordError: ''
         // names: []
     }
 },
 methods: {
+    addSkill(e){
+        // if this.tempSkill is still empty it will return false!
+        if(e.key === ',' && this.tempSkill) {
+            if(!this.skills.includes(this.tempSkill)) {
+                this.skills.push(this.tempSkill)
+            }
+            this.tempSkill = ''
+        }
+        console.log(this.skills)
+    },
+    erase(skill){
+        this.skills = this.skills.filter((item) => {return skill !== item})
+    },
+    handleSubmit(){
+        // validate password
+        this.passwordError = this.password.length >= 6 ?
+        '' : 'the password has to be at least 6 chars long!'
+
+        if(!this.passwordError) {
+            console.log('email', this.email)
+            console.log('password', this.password)
+            console.log('role', this.role)
+            console.log('skills', this.skills)
+            console.log('terms accepted', this.terms)
+        }
+    }
 }
 }
 </script>
@@ -90,5 +130,36 @@ input[type="checkbox"] {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+.pill {
+    display: inline-block;
+    margin: 20px;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+button{
+    background: #0b6dff;
+    border: none;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+    cursor: pointer;
+}
+.submit{
+    text-align: center;
+}
+.error {
+    background: rgb(235, 57, 44);
+    border-radius: 4px;
+    text-align: center;
+    margin-top: 5px;
+    color: white;
 }
 </style>
